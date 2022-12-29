049d56e52342fe4d71da8324e86ce0dc521a1f5e
yenalena <lena@petpharm.co.kr>
2022-12-29 11:26:45 +0900

    Initial Commit

---
+++ pet_care_app/README.md
@@ -0,0 +1,4 @@
+# pet_care_app
+
+https://dribbble.com/shots/13441399-Pet-Care-App-Concept
+
---
+++ pet_care_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:pet_care_app/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    return ScreenUtilInit(
+      designSize: const Size(360, 720),
+      builder: (context, child) => GetMaterialApp(
+        title: 'Pet Care App',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+  }
+}
