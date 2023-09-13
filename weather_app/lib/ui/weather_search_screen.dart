d28114e90ca2009dc53b894cf021e28f60bb80e0
yenalena <lena@petpharm.co.kr>
2023-09-13 15:44:56 +0900

    initial commit

---
+++ weather_app/lib/ui/weather_search_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class WeatherSearchScreen extends StatelessWidget {
+  const WeatherSearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
